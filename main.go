81fe65376f299b68f1ce46c7debf49320364dc9e
Geoffrey Callaghan <callagg3@gmail.com>
2019-07-14 16:15:59 -0400

    Update main.go

--- main.go
+++ main.go
@@ -122,7 +122,7 @@ cur.Close(context.TODO())
 //fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
 
 for _, *title := range results {
-      fmt.Printf("address ",title)
+      fmt.Printf("address ",*title)
 }
 
 }
