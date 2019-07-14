3de434737f3ba0209f8da5b3fafe73b2e4203428
Geoffrey Callaghan <callagg3@gmail.com>
2019-07-14 17:39:27 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import(
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
-)
+
+go.mongodb.org/mongo-driver/bson/primitive"
+ }
 
 /*
+
 type OtherName struct {
     TitleNameLanguage string
     TitleNameType string
