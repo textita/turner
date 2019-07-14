381584f59052d5c78661e30dafcc1464dc573836
Geoffrey Callaghan <callagg3@gmail.com>
2019-07-14 17:41:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import(
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
-
-go.mongodb.org/mongo-driver/bson/primitive"
- }
+     go.mongodb.org/mongo-driver/bson/primitive"
+ )
 
 /*
 
