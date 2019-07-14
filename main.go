8935e85b44afb6771149db4017b9b87d887bbcaf
Geoffrey Callaghan <callagg3@gmail.com>
2019-07-14 11:30:57 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,37 +9,6 @@ import(
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-  
-connStr:= "mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge"
-  
-// Set client options
-clientOptions := options.Client().ApplyURI(connStr)
-
-// Connect to MongoDB
-client, err := mongo.Connect(context.TODO(), clientOptions)
-
-if err != nil {
-    log.Fatal(err)
-}
-
-// Check the connection
-err = client.Ping(context.TODO(), nil)
-
-if err != nil {
-    log.Fatal(err)
-}
-
-  
-fmt.Println("Connected to MongoDB!")
-
-  collection := client.Database("dev-challenge").Collection("titles")
-  
-  cur, err := collection.Find(context.TODO(), bson.D{{}})
-
-if err != nil {
-    log.Fatal(err)
-}
 
 type otherName struct {
     TitleNameLanguage string
@@ -74,7 +43,8 @@ type storyLine struct {
  Award  string
  AwardCompany  string 
 }
-  type title struct {
+
+type title struct {
   Id  string
   Awards  []award
   Genres  []string
@@ -84,7 +54,38 @@ type storyLine struct {
 
   }
 
-  func main() {
+func main() {
+  
+connStr:= "mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge"
+  
+// Set client options
+clientOptions := options.Client().ApplyURI(connStr)
+
+// Connect to MongoDB
+client, err := mongo.Connect(context.TODO(), clientOptions)
+
+if err != nil {
+    log.Fatal(err)
+}
+
+// Check the connection
+err = client.Ping(context.TODO(), nil)
+
+if err != nil {
+    log.Fatal(err)
+}
+
+  
+fmt.Println("Connected to MongoDB!")
+
+  collection := client.Database("dev-challenge").Collection("titles")
+  
+  cur, err := collection.Find(context.TODO(), bson.D{{}})
+
+if err != nil {
+    log.Fatal(err)
+}
+
   fmt.Println("hello")
 
 // Finding multiple documents returns a cursor
